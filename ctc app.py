a6961d7190cdd189fb71f3378c2bc9d1f42f10bc
vishwakarthik69-dev <vishwakarthik69@gmail.com>
2026-03-28 15:58:01 +0530

    Add files via upload

---
+++ ctc app.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, request
+import requests, os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/check', methods=['POST'])
+def check():
+    ip = request.form.get('ip')
+    print("User entered IP:",ip)
+
+    url = f"https://www.virustotal.com/api/v3/ip_addresses/{ip}"
+    headers = {"x-apikey": os.getenv("VT_API_KEY")}
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        stats = data['data']['attributes']['last_analysis_stats']
+
+        malicious = stats['malicious']
+        suspicious = stats['suspicious']
+        harmless = stats['harmless']
+
+        if malicious > 5:
+            threat = "HIGH"
+        elif suspicious > 0:
+            threat = "MEDIUM"
+        else:
+            threat = "LOW"
+
+        return render_template(
+            "index.html",
+            ip=ip,
+            malicious=malicious,
+            suspicious=suspicious,
+            harmless=harmless,
+            threat=threat
+        )
+    else:
+        return "Error fetching data"
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+
